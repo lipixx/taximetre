16c5315826eabb585bd7763be7bfdff778136b19
lipixx <lipixx@9552da3e-b024-11dd-b6fe-af671ef85443>
2008-11-11 20:32:32 +0000

    First Commit

---
+++ include/constants.h
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////
+///Fitxer de definicio de constants.
+///Practica Final SDMI - QT2008
+///
+///Felip Moll
+///Robert Millan
+///////////////////////////////////////////////////////////////////
+
+#define LLIURE 0
+#define REPOS 1
+#define CONTROLS 2
+#define OCUPAT 3
+#define IMPORT 4
+
+#define ON 1
+#define OFF 0
