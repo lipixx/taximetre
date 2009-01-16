bc11b88f5efca5f1ccec1c51f785f01cc69ce26d
rmh@aybabtu.com <rmh@aybabtu.com@9552da3e-b024-11dd-b6fe-af671ef85443>
2009-01-16 14:16:39 +0000

    Afegir llicència a la capçalera

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
-///////////////////////////////////////////////////////////////////
-///Codi font del Taximetre
-///Pràctica Final SDMI - QT2008
-///
-///Felip Moll - lipixx at gmail.com
-///Robert Millan - robert.millan at est.fib.upc.edu
-///Sota llicència GPLv3
-///16/01/2009
-///////////////////////////////////////////////////////////////////
+/*
+ * Taxímetre -- Pràctica Final SDMI - QT2008
+ * Copyright (C) 2008,2009  Felip Moll <lipixx at gmail.com>
+ * Copyright (C) 2008,2009  Robert Millan <rmh@aybabtu.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "include/16F876_CCS.h"
 #include "include/constants.h"
